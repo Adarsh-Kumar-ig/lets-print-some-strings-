8364102ea8638947b01596f17640d0821f4c33de
Adarsh-Kumar-ig <namankumarsr1@gmail.com>
2025-08-05 09:57:53 -0400

    Add files via upload

---
+++ LET'S PRINT SOME STRINGS.py
@@ -0,0 +1,6 @@
+'''LET'S PRINT SOME STRINGS'''
+
+a = "Hello world"     
+'''(here a is the variable name where the string data will store)'''
+print(a)
+'''(here the print() function displays output on the screen) '''
